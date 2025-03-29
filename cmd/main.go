ee37ab2c7d1d13036c8d9a2344bf1396ab59b55c
ArtemS <savenkoam@gmail.com>
2025-03-29 22:18:43 +0900

    create config/env.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"go-fiber-job/config"
 	"go-fiber-job/internal/home"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
 	app := fiber.New()
 
 	home.NewHandler(app) // вызов handler-а internal/home
