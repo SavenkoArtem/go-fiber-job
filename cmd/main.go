e894daf0e057b8673fe2c71a0ad0f2639f9775fc
ArtemS <savenkoam@gmail.com>
2025-04-06 11:40:15 +0900

    Template

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,12 @@ import (
 	"go-fiber-job/config"
 	"go-fiber-job/internal/home"
 	"go-fiber-job/pkg/logger"
+	"strings"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -15,8 +17,16 @@ func main() {
 	config.NewDatabaseConfig()
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
+	engine := html.New("./html", ".html") // в указанной дериктории парсит все файлы с выбранным расширением
+	engine.AddFuncMap(map[string]interface{}{
+		"ToUpper": func(c string) string {
+			return strings.ToUpper(c)
+		},
+	})
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Views: engine, // это temple engin который работает с шаблонам
+	})
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
